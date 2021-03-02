566ff4ba7e176fe21d66ab2732cf09ad87ad2b33
ndonmez <info@ndonmez.com>
2021-03-02 22:46:26 +0300

    Update laraveltestServiceProvider.php

--- src/laraveltestServiceProvider.php
+++ src/laraveltestServiceProvider.php
@@ -2,7 +2,19 @@
 
 namespace NDONMEZ\laraveltest;
 
-
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Str;
 
 class laraveltestServiceProvider extends ServiceProvider
 {
